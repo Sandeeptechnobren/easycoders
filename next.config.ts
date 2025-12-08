d4413d2e9174348e1513c3819a4e332a2bf51543
Sandeep <sandeep@buildcompany.co>
2025-12-08 14:07:45 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ✅ Prevent ESLint from failing production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
