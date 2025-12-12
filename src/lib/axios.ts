e6dc72d297a7916506d6ca4f6a30ea5482ce950e
Sandeep <sandeep@buildcompany.co>
2025-12-12 17:56:21 +0530

    New

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.easycoders.in/backend/public/api',
+    baseURL: 'https://api.easycoders.in/projects/backend/public/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
